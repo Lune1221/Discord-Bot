eac9afec6e3a3d2a267e7900ecae69023209dbd8
Lune1221 <bell12251221@gmail.com>
2026-07-30 22:07:26 +0900

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -1,22 +1,19 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    // /gif コマンドの定義
     data: new SlashCommandBuilder()
         .setName('gif')
         .setDescription('GIF埋め込みを表示します'),
         
     async execute(interaction) {
-        // 埋め込みの作成
         const embed = new EmbedBuilder()
             .setTitle('GIFテスト（このすば）')
             .setColor(0xFF0000)
-            // ご提示いただいたTenorのGIF URLを設定
-            .setImage('https://tenor.com');
+            // 👇 ここを上記の「://tenor.com」から始まるURL、またはGiphyのURLに変更します
+            .setImage('https://://tenor.com/hsYNUQdAYeoAAAAC/megumin-explosion.gif');
 
-        // スラッシュコマンドに対して返信する
-        // もし事前に deferReply() しているなら、reply ではなく editReply を使う
-await interaction.editReply({ embeds: [embed] });
-
+        // すでにエラーが解消して通常の返信ができる場合は reply
+        // もしハンドラー側で deferReply している場合は editReply にしてください
+        await interaction.reply({ embeds: [embed] });
     },
 };
